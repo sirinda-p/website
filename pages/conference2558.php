1fec45297d85956302b5c3af4954ba7db3bf7784
Sirinda <sirinda111@gmail.com>
2017-01-06 15:01:00 +0700

    add temporary conference pages

---
+++ pages/conference2558.php
@@ -0,0 +1,24 @@
+<?php include_once("analyticstracking.php") ?>
+
+<section id="section-contact" class="section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 wow fadeInDown">
+                <div class="section-heading text-center">
+                    <h2 class="section-title">&nbsp;</h2>
+                    <h2 class="section-title"><span class="wow slideInLeft">งานประชุมวิชาการ</span> 2558</h2>
+                    <hr>
+              </div>
+            </div>
+        </div> <!-- row end -->
+        <div class="row">
+            <div class="col-md-12 wow bounceInUp">
+              <div class="section-heading text-center">
+                  <div align="left"><a href="http://social.utcc.ac.th/"><img src="images/conferrentpic/ictm2014.jpg" alt="" width="32" height="32" border="0" align="bottom" class="img-responsive"></a>                  </div>
+                 
+                  </p>
+</div>
+          </div>
+        </div>
+      </div>
+    </section><!-- section end -->
---
+++ pages/conference_main.php
@@ -0,0 +1,14 @@
+<?php include_once("analyticstracking.php") ?>
+
+<section id="section-conference" class="section-padding">
+    <div class="row">
+            <div class="col-md-12 wow bounceInUp">
+                <div class="section-heading text-center">
+				 </br></br> 
+                    <h3>Coming soon</h3>
+					 
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </section><!-- section end -->
