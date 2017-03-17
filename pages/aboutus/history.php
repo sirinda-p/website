2857a9e944df01e3c2fd83053005b1a077bd8c86
babyapt <babyapt@hotmail.com>
2017-03-17 16:12:42 +0700

    แก้ไขหน้าประวัติความเป็นมา

--- pages/aboutus/history.php
+++ pages/aboutus/history.php
@@ -1,16 +1,17 @@
 <?php include_once("analyticstracking.php") ?>
- 
+<?php
+/*
 <div class="section section-white" id="section-slider">
   <div class="container">
     <div class="row">
         <div class="col-md-12">
 
-            </br></br> 
-            </br></br> 
-            </br></br> 
-              
+            </br></br>
+            </br></br>
+            </br></br>
+
 			  <h2>ประวัติความเป็นมาของคณะวิชา </h2>
-          
+
 		  <p>คณะวิทยาศาสตร์ได้จัดตั้งขึ้นในปี พ.ศ.2527 และได้รับการอนุมัติให้ปรับเปลี่ยนชื่อคณะฯ เป็น คณะวิทยาศาสตร์และเทคโนโลยี ในปี
 พ.ศ. 2554 คณะฯ ได้ผลิตบัณฑิตในสาขาวิชาต่างๆ จนถึงปัจจุบันรวมทั้งสิ้น 29 รุ่น โดยมีสาขาวิชาต่างๆ ดังนี้ </p>
 			<p> -  พ.ศ.2527 เปิดสาขาวิชาคณิตศาสตร์ สาขาวิชาสถิติประยุกต์ และสาขาวิชาวิทยาการคอมพิวเตอร์ </p>
@@ -31,13 +32,285 @@
 			<p>- พ.ศ.2556 เปิดหลักสูตรปริญญาตรีสาขาวิชาเทคโนโลยีการพัฒนาผลิตภัณฑ์อุตสาหกรรมเกษตร และปรับปรุงหลักสูตรปริญญาโทสาขาวิชาการจัดการเทคโนโลยีสารสนเทศและการสื่อสาร   </p>
 			<p>- พ.ศ. 2557 เปิดหลักสูตรปริญญาโทสาขาวิชาคณิตศาสตร์และเทคโนโลยีสารสนเทศ  และปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาคอมพิวเตอร์แอนิเมชั่น    </p>
 			<p> - พ.ศ. 2558 ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาเทคโนโลยีสารสนเทศและการสื่อสารและปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาการจัดการธุรกิจอาหาร    </p>
-		 
-             </br></br> 
+
+             </br></br>
 
              </div>
-			
-			
+
+
         </div>
     </div>
   </div>
 </div>
+*/?>
+<section id="resume" class="resume-section section-padding" style="padding-top:150px;">
+		<div class="container">
+				<h2 class="section-title wow fadeInUp">ประวัติความเป็นมาของคณะวิชา</h2>
+				<div class="row">
+						<div class="col-md-12">
+								<div class="resume-title">
+										<h3>คณะวิทยาศาสตร์ได้จัดตั้งขึ้นในปี พ.ศ.2527 และได้รับการอนุมัติให้ปรับเปลี่ยนชื่อคณะฯ เป็น คณะวิทยาศาสตร์และเทคโนโลยี ในปีพ.ศ. 2554 คณะฯ ได้ผลิตบัณฑิตในสาขาวิชาต่างๆ จนถึงปัจจุบันรวมทั้งสิ้น 29 รุ่น โดยมีสาขาวิชาต่างๆ ดังนี้</h3>
+								</div>
+								<div class="resume">
+										<ul class="timeline">
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2527</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาคณิตศาสตร์</h4>
+																				<h4>เปิดสาขาวิชาสถิติประยุกต์</h4>
+																				<h4>เปิดสาขาวิชาวิทยาการคอมพิวเตอร์</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2529</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาวิทยาการคอมพิวเตอร์ (ภาคค่ำ)</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2532</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาวิทยาศาสตร์การอาหาร</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2533</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาเทคโนโลยีอุตสาหกรรมอาหาร</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2537</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสอนในระดับบัณฑิตศึกษา</h4>
+																				<span>สาขาวิชาการจัดการเทคโนโลยีสารสนเทศ</span>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2539</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาเทคโนโลยีสารสนเทศ</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2547</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>รวมสาขาวิชาวิทยาศาสตร์การอาหาร และสาขาวิชาเทคโนโลยีอุตสาหกรรมอาหาร</h4>
+																				<span>โดยเปลี่ยนชื่อเป็น สาขาวิชาวิทยาศาสตร์และเทคโนโลยีการอาหาร</span>
+																				<h4>ปรับปรุงหลักสูตรระดับบัณฑิตศึกษา</h4>
+																				<span>โดยเปลี่ยนชื่อจาก สาขาวิชาการจัดการเทคโนโลยีสารสนเทศ เป็นสาขาวิชาการจัดการเทคโนโลยีสารสนเทศและการสื่อสาร</span>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2547</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาการจัดการอุตสาหกรรมอาหาร (CEO MBA)</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2548</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาคณิตศาสตร์การเงิน</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2549</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดสาขาวิชาคอมพิวเตอร์แอนิเมชั่น</h4>
+																				<h4>เปิดสาขาวิชาระบบอุตสาหกรรมอาหาร</h4>
+																				<h4>เปิดสาขาวิชาการจัดการไฮเทค (CEO MBA)</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2551</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาเทคโนโลยีสารสนเทศ</h4>
+																				<span>โดยเปลี่ยนชื่อเป็นสาขาวิชาเทคโนโลยีสารสนเทศและการสื่อสาร</span>
+																				<h4>ปรับปรุงหลักสูตร CEO MBA สาขาวิชาการจัดการไฮเทค</h4>
+																				<span>โดยเปลี่ยนชื่อเป็นกลุ่มวิชาการจัดการเทคโนโลยีและนวัตกรรม</span>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2552</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาระบบอุตสาหกรรมอาหาร</h4>
+																				<span>โดยเปลี่ยนชื่อเป็นสาขาวิชาการจัดการธุรกิจอาหาร</span>
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาคณิตศาสตร์การเงิน</h4>
+																				<span>โดยเปลี่ยนชื่อเป็นสาขาวิชาวิศวกรรมการเงิน และเป็นหลักสูตร 4 + 1</span>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2553</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดหลักสูตรปริญญาโทสาขาวิชาวิศวกรรมการเงิน</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2554</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดหลักสูตรปริญญาตรีสาขาวิชาการออกแบบเว็บและเนื้อหา</h4>
+																				<h4>ปรับเปลี่ยนชื่อคณะฯ</h4>
+																				<span>จากเดิม คณะวิทยาศาสตร์ เป็นคณะวิทยาศาสตร์และเทคโนโลยี</span>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2555</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาการออกแบบเว็บและเนื้อหา</h4>
+																				<span>โดยเปลี่ยนชื่อเป็นสาขาวิชาเว็บและโมบายเทคโนโลยี</span>
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาวิทยาการคอมพิวเตอร์ (4+1)</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2556</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดหลักสูตรปริญญาตรีสาขาวิชาเทคโนโลยีการพัฒนาผลิตภัณฑ์อุตสาหกรรมเกษตร</h4>
+																				<h4>ปรับปรุงหลักสูตรปริญญาโทสาขาวิชาการจัดการเทคโนโลยีสารสนเทศและการสื่อสาร</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li>
+														<div class="posted-date">
+																<span class="month">พ.ศ.2557</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>เปิดหลักสูตรปริญญาโทสาขาวิชาคณิตศาสตร์และเทคโนโลยีสารสนเทศ</h4>
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาคอมพิวเตอร์แอนิเมชั่น</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+												<li class="timeline-inverted">
+														<div class="posted-date">
+																<span class="month">พ.ศ.2558</span>
+														</div><!-- /posted-date -->
+
+														<div class="timeline-panel wow fadeInUp">
+																<div class="timeline-content">
+																		<div class="timeline-heading">
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาเทคโนโลยีสารสนเทศและการสื่อสาร</h4>
+																				<h4>ปรับปรุงหลักสูตรปริญญาตรีสาขาวิชาการจัดการธุรกิจอาหาร</h4>
+																		</div><!-- /timeline-heading -->
+																</div> <!-- /timeline-content -->
+														</div><!-- /timeline-panel -->
+												</li>
+										</ul>
+								</div>
+						</div>
+				</div><!-- /row -->
+		</div><!-- /.container -->
+</section><!-- End Resume Section -->
